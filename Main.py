ff8ac5db5d22b34af83feef4e14f3f25de5cf639
Sean <91260510+seanwhitee@users.noreply.github.com>
2023-05-07 22:29:13 +0800

    Change back to script version.

--- Main.py
+++ Main.py
@@ -1,28 +1,19 @@
 from pytube import YouTube
 import ssl
-import streamlit as st
-
 
 def main():
     ssl._create_default_https_context = ssl._create_stdlib_context
     
-    st.markdown("""# Download Youtube Video""")
+        
+    # User input url
+    url = input("Youtube video url: ")
+    download_path = input("Download path: ")
     
-    with st.form("my_form"):
-        
-        # User input url
-        url = st.text_input(label='URL', placeholder='https://www.youtube.com/...')
-        download_path = st.text_input(label='Download Path', placeholder='/Users/sean/Desktop/downloaded-video')
-        
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            
-            # Create youtube object
-            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
-            stream = yt.streams.get_highest_resolution()
-            stream.download(f"{download_path}")
-            st.success(f"Successfully download {yt.title} to {download_path}")
-                
+    
+    # Create youtube object
+    yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+    stream = yt.streams.get_highest_resolution()
+    stream.download(f"{download_path}")
             
                 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # YoutubeDownloader 
 
-[![Made with streamlit](https://img.shields.io/badge/Made%20with-streamlit-orange)](https://streamlit.io/)
 [![MIT License](https://img.shields.io/badge/license-MIT-red)](https://github.com/seanwhitee/YoutubeDownloader/blob/main/license)
 [![Made with python](https://img.shields.io/badge/Made%20with-Python-brightgreen)](https://www.python.org/)
 
@@ -10,7 +9,6 @@ This is a simple python script to download youtube video via url.
 
 ## Dependency
 - pytube
-- streamlit
 
 ## Usage
 1. Create a venv for your project.
@@ -23,17 +21,11 @@ env/Scripts/activate # Windows
 2. Install dependencies.
 ```bash
 pip install pytube
-pip install streamlit
 ```
 
 3. Run project
 
-- If you want visualize using browser.
-```bash
-streamlit run Main.py
-```
 - If you want to run with python script
-- Modify the code  in the Main.py file. (using terminal based input instead of streamlit brower based input).
 - And using following command to run the script.
 
 ```bash
